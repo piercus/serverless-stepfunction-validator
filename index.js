7b635bbc1f23d28145b2a1d4f3ccd796195c64dd
Pierre Colle <piercus@gmail.com>
2019-06-04 18:02:59 +0200

    linting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const aslValidator = require('asl-validator');
 const path = require('path');
+const aslValidator = require('asl-validator');
 const BbPromise = require('bluebird');
 
 class ServerlessStepfunctionValidator {
@@ -24,61 +24,61 @@ class ServerlessStepfunctionValidator {
 	}
 
 	yamlParse() {
-    const servicePath = this.serverless.config.servicePath;
-    if (!servicePath) {
-      return BbPromise.resolve();
-    }
+		const {servicePath} = this.serverless.config;
+		if (!servicePath) {
+			return BbPromise.resolve();
+		}
 
-    const serverlessYmlPath = path.join(servicePath, 'serverless.yml');
-    return this.serverless.yamlParser
-      .parse(serverlessYmlPath)
-      .then(serverlessFileParam =>
-        this.serverless.variables.populateObject(serverlessFileParam)
-        .then(parsedObject => {
-          this.serverless.service.stepFunctions = {};
-          this.serverless.service.stepFunctions.stateMachines
-              = parsedObject.stepFunctions
-            && parsedObject.stepFunctions.stateMachines
-              ? parsedObject.stepFunctions.stateMachines : {};
-          this.serverless.service.stepFunctions.activities
-              = parsedObject.stepFunctions
-            && parsedObject.stepFunctions.activities
-              ? parsedObject.stepFunctions.activities : [];
+		const serverlessYmlPath = path.join(servicePath, 'serverless.yml');
+		return this.serverless.yamlParser
+			.parse(serverlessYmlPath)
+			.then(serverlessFileParam =>
+				this.serverless.variables.populateObject(serverlessFileParam)
+					.then(parsedObject => {
+						this.serverless.service.stepFunctions = {};
+						this.serverless.service.stepFunctions.stateMachines =
+							parsedObject.stepFunctions &&
+						parsedObject.stepFunctions.stateMachines ?
+								parsedObject.stepFunctions.stateMachines : {};
+						this.serverless.service.stepFunctions.activities =
+							parsedObject.stepFunctions &&
+						parsedObject.stepFunctions.activities ?
+								parsedObject.stepFunctions.activities : [];
 
-          if (!this.serverless.pluginManager.cliOptions.stage) {
-            this.serverless.pluginManager.cliOptions.stage = this.options.stage
-            || (this.serverless.service.provider && this.serverless.service.provider.stage)
-            || 'dev';
-          }
+						if (!this.serverless.pluginManager.cliOptions.stage) {
+							this.serverless.pluginManager.cliOptions.stage = this.options.stage ||
+						(this.serverless.service.provider && this.serverless.service.provider.stage) ||
+						'dev';
+						}
 
-          if (!this.serverless.pluginManager.cliOptions.region) {
-            this.serverless.pluginManager.cliOptions.region = this.options.region
-            || (this.serverless.service.provider && this.serverless.service.provider.region)
-            || 'us-east-1';
-          }
+						if (!this.serverless.pluginManager.cliOptions.region) {
+							this.serverless.pluginManager.cliOptions.region = this.options.region ||
+						(this.serverless.service.provider && this.serverless.service.provider.region) ||
+						'us-east-1';
+						}
 
-          this.serverless.variables.populateService(this.serverless.pluginManager.cliOptions);
-          return BbPromise.resolve();
-        })
-      );
-  }
+						this.serverless.variables.populateService(this.serverless.pluginManager.cliOptions);
+						return BbPromise.resolve();
+					})
+			);
+	}
 
 	validate() {
-    this.yamlParse().then(() => {
-  		let allErrors = [];
-  		for (const k of Object.keys(this.serverless.service.stepFunctions.stateMachines)) {
-  			const stateMachineDefinition = this.serverless.service.stepFunctions.stateMachines[k].definition;
-  			const {isValid, errors} = aslValidator(stateMachineDefinition);
-  			allErrors = allErrors.concat(errors);
-  		}
-  		if (allErrors.length > 0) {
-        allErrors.forEach(err => {
-          this.serverless.cli.log(JSON.stringify(err, null, 2));
-        })
-  			return Promise.reject(new Error('not valid asl'));
-  		}
-  		return Promise.resolve({});
-    });
+		this.yamlParse().then(() => {
+			let allErrors = [];
+			for (const k of Object.keys(this.serverless.service.stepFunctions.stateMachines)) {
+				const stateMachineDefinition = this.serverless.service.stepFunctions.stateMachines[k].definition;
+				const {errors} = aslValidator(stateMachineDefinition);
+				allErrors = allErrors.concat(errors);
+			}
+			if (allErrors.length > 0) {
+				allErrors.forEach(err => {
+					this.serverless.cli.log(JSON.stringify(err, null, 2));
+				});
+				throw new Error('not valid asl');
+			}
+			return {};
+		});
 	}
 }
 
